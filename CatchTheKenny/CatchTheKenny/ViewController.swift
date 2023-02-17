5a1af560e4636a2b3d1f221127513711ba17c45e
keremkiyak <110934008+keremkiyak@users.noreply.github.com>
2023-02-17 14:23:47 +0300

    Add files via upload

---
+++ CatchTheKenny/CatchTheKenny/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CatchTheKenny
+//
+//  Created by kerem on 17.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    @IBOutlet weak var kenny1: UIImageView!
+    @IBOutlet weak var kenny2: UIImageView!
+    @IBOutlet weak var kenny3: UIImageView!
+    @IBOutlet weak var kenny4: UIImageView!
+    @IBOutlet weak var kenny5: UIImageView!
+    @IBOutlet weak var kenny6: UIImageView!
+    @IBOutlet weak var kenny7: UIImageView!
+    @IBOutlet weak var kenny8: UIImageView!
+    @IBOutlet weak var kenny9: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
